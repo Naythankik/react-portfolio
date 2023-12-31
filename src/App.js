6dff48db87e230d50af320d4d9dba6ea9010c233
Naythankik <nathabolarin@gmail.com>
2023-12-31 17:46:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const images = [
+    'bootstrap', 'html', 'php', 'tailwind', 'mongo',
+     'git', 'mysql', 'heroku', 'netlify', 'css',
+      'js', 'node', 'github', 'laravel', 'react', 'postman'
+  ];
+  const description = (
+    <p>
+      I am a Software Engineer with a foundational understanding of
+      designing and implementing new features from concept to production. I
+      prioritize user experience while developing reusable and efficient
+      code. I am dedicated to merging good design, technology, and
+      innovation in every project I undertake, from inception to launch.
+      Currently, my focus lies primarily on backend development, with some
+      experience in frontend development.
+    </p>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myself">
+    <h1>About Me</h1>
+    
+    <h3 style={{ marginBottom: 20 }}>A bit about me</h3>
+    { description }
+    <h3 style={{ marginTop: 20 }}>Technology and Tools</h3>
+
+    <p>
+      Using a combination of cutting-edge technologies and reliable
+      open-source software I build user-focused, performant websites for
+      smartphones, tablets, and desktops.
+    </p>
+    
+    <div className="packages">
+
+      {images.map((image) => (
+        <div className="tool">
+          <img src={ `./images/${image}.svg` } alt="#" /> <span>{ image.toUpperCase() }</span>
+
+        </div>
+        ))}
+
     </div>
+
+  </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return ( 
+        <div class="intro">
+        <h1>Hi, I am Nathaniel</h1>
+        <h5>A Backend Developer.</h5>
+      </div>
+     );
+}
+ 
+export default Header;
